7bc26a3759fae3cf406736f18e5b01866e3d7609
GhostedGaming <epicrobloxgamer41@gmail.com>
2026-08-16 23:45:31 -0400

    tty: add backbuf, render_target, and backbuf_mode fields to struct tty

--- kernel/include/tty.h
+++ kernel/include/tty.h
@@ -21,6 +21,10 @@
 #define VTIME   6
 #define NCCS    8
 
+/* Coarse APIC-timer rate used to convert VTIME (tenths of a second) to
+   scheduler ticks.  The real rate is roughly 60-120 ticks/sec on QEMU. */
+#define TIMER_TICKS_PER_SEC 100
+
 typedef struct {
     uint32_t c_iflag;
     uint32_t c_oflag;
@@ -66,6 +70,9 @@ struct tty {
     int        esc_nparam;
     uint32_t   fg;
     uint32_t   bg;
+    uint32_t  *backbuf;
+    uint32_t  *render_target;
+    int        backbuf_mode;
 };
 
 struct termios_user {
